7f3986af65b35e1b54391585f6789b1a6c86ac41
EC2 Default User <ec2-user@ip-172-31-22-12.ap-northeast-1.compute.internal>
2022-12-15 12:51:09 +0000

    [Finish]4-24

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,11 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
